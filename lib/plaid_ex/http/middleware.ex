e45fbace55876192cefd3adfb4a8b950cf62e10c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-08-08 12:05:08 +0530

    feat: add middleware

---
+++ lib/plaid_ex/http/middleware.ex
@@ -0,0 +1,8 @@
+defmodule PlaidEx.HTTP.Middleware do
+  @moduledoc """
+  Namespace for PlaidEx HTTP middleware modules.
+
+  Middleware is applied in the HTTP client pipeline. Each middleware
+  transforms requests or handles responses.
+  """
+end
